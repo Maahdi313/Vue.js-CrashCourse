a6a33069d95de505a91216b7ca6c168c70bcd63b
Maahdi313 <mr.farhad112@gmail.com>
2024-02-22 17:25:48 +0100

    Vue crash course
    project with vue.js

---
+++ src/components/TodoButton.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <slot>Create</slot>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+button {
+    padding: 8px 16px;
+    border: none;
+  }
+</style>
